663fe6c1fccfd597d1ca95a59a4af7805e652fbc
Bishanpreet signh <bishanpreet345@gmail.com>
2025-05-24 05:50:05 +0530

    Update Preloader.tsx

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
-import anime from 'animejs';
 
 const Preloader = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -8,7 +7,7 @@ const Preloader = () => {
   useEffect(() => {
     // Prevent background scrolling
     document.body.style.overflow = 'hidden';
-
+    
     if (videoRef.current) {
       videoRef.current.playbackRate = 1.8;
     }
@@ -25,7 +24,7 @@ const Preloader = () => {
       exit={{ opacity: 0 }}
       className="fixed inset-0 z-50 flex items-center justify-center bg-black"
     >
-      <div className="relative w-[880px] mx-auto">
+      <div className="relative w-[880px] mx-auto"> 
         <video
           ref={videoRef}
           src="https://res.cloudinary.com/dbtj6orw2/video/upload/v1745685398/unyzy7z0deaokc9mpq1m.mp4"
@@ -39,4 +38,4 @@ const Preloader = () => {
   );
 };
 
-export default Preloader
+export default Preloader;
