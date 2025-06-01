987d4205596429950fc3d4ebd790bad2de888272
Bishanpreet signh <bishanpreet345@gmail.com>
2025-06-01 19:12:51 +0000

    m

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -207,7 +207,7 @@ const Navbar = () => {
               </Link>
             ))}
             <GlassMorphButton
-              to="/"
+              to="/courses"
               className="px-5 py-2 text-white text-base rounded-full flex items-center gap-1 relative"
             >
               <NewBadge className="absolute -top-4 -right-2">
@@ -289,7 +289,7 @@ const Navbar = () => {
                   }}
                 >
                   <Link
-                    to="/"
+                    to="/courses"
                     className="flex items-center gap-3 text-2xl py-3 text-white group"
                     onClick={() => setIsOpen(false)}
                   >
