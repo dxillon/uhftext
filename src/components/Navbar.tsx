55c09ef25d96d89ff1d9216bba737e5515ae16fe
Bishanpreet signh <bishanpreet345@gmail.com>
2025-06-10 01:35:22 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -207,7 +207,7 @@ const Navbar = () => {
             })}
 <GlassMorphButton
   to="/courses"
-  className=" grouppx-5 py-2 text-[22px] font-medium text-white text-base rounded-full flex items-center gap-1.5 relative"
+  className=" group px-5 py-2 text-[22px] font-medium text-white text-base rounded-full flex items-center gap-1.5 relative"
 >
   <LibraryBig className="transition-colors duration-300 group-hover:text-red-400 text-white text-[26px]" />
   <span className="leading-tight text-[22px]">Courses</span>
