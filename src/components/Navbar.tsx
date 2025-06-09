8fddcb168dd1512758a39e44d451f90e567c13db
Bishanpreet signh <bishanpreet345@gmail.com>
2025-06-10 01:45:32 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -190,7 +190,7 @@ const Navbar = () => {
           </div>
 
           {/* Desktop Nav */}
-          <div className="hidden md:flex items-center space-x-10 -mr-6">
+          <div className="hidden md:flex items-center space-x-10 mr-6">
             {navLinks.map((link) => {
               const Icon = link.icon;  
               return (  
@@ -207,7 +207,7 @@ const Navbar = () => {
             })}
 <GlassMorphButton
   to="/courses"
-  className=" group px-3 py-2.5 text-[22px] font-medium text-white text-base rounded-full flex items-center pr-5 gap-1.5 relative"
+  className=" group px-3 py-2.5 text-[22px] font-medium text-white text-base rounded-full flex items-center  gap-1.5 relative"
 >
   <LibraryBig className="transition-colors duration-300 group-hover:text-red-400 " />
   <span className="leading-tight text-[22px]">Courses</span>
