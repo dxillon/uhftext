2e3fca32b054afd1fa5d740a560a091ff0621592
Bishanpreet signh <bishanpreet345@gmail.com>
2025-06-10 07:10:40 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -227,7 +227,7 @@ const CircleArrowOutUpRight = ({
                       <span className="text-white font-medium text-base lg:text-lg whitespace-nowrap overflow-hidden text-ellipsis mr-2 group-hover:text-red-400 transition-colors">
                         {item.text}
                       </span>
-                      <CircleArrowOutUpRight className="w-6 h-6 text-red-400 flex-shrink-0 ml-2" />
+                      <CircleArrowOutUpRight className="width={16} height={16 text-red-400 flex-shrink-0 ml-2" />
                     </Link> 
                   </div>
                 ))}
