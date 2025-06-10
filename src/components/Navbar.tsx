1826192d0fd7e2f22e80cdfb46ffa7ee91cbbdb6
Bishanpreet signh <bishanpreet345@gmail.com>
2025-06-10 06:32:30 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -224,7 +224,7 @@ const Navbar = () => {
                   className={`group flex flex-col items-center text-white transition-colors hover:text-red-400 ${location.pathname === link.path ? 'text-red-500 font-semibold' : ''
                     }`}
                 >
-                  <Icon style={link.style} className="w-6 h-6" />
+                  <Icon  className="w-6 h-6" />
                   <span
                     className="  text-[12px] mt-1 font-medium  tracking-wider  leading-relaxed   font-sans  text-white  drop-shadow-md  "
                   >
