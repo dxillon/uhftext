bd39b50794f9c00772ec5d5a1f4cdd1b8bb3f13e
Bishanpreet signh <bishanpreet345@gmail.com>
2025-06-10 07:09:51 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,10 +45,32 @@ const Navbar = () => {
   const logoRef = useRef<HTMLDivElement>(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const CircleArrowOutUpRight = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#f87171" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-out-up-right-icon lucide-circle-arrow-out-up-right"><path d="M22 12A10 10 0 1 1 12 2" /><path d="M22 2 12 12" /><path d="M16 2h6v6" /></svg>
-  );
-
+const CircleArrowOutUpRight = ({ 
+  width = 24, 
+  height = 24,
+  stroke = "#f87171",
+  strokeWidth = 2,
+  className = "",
+  ...props 
+}: React.SVGProps<SVGSVGElement>) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    width={width}
+    height={height}
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke={stroke}
+    strokeWidth={strokeWidth}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={`lucide lucide-circle-arrow-out-up-right-icon ${className}`}
+    {...props}
+  >
+    <path d="M22 12A10 10 0 1 1 12 2" />
+    <path d="M22 2 12 12" />
+    <path d="M16 2h6v6" />
+  </svg>
+);
 
 
     const Projector = (props: React.SVGProps<SVGSVGElement>) => (
